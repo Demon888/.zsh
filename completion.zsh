32f72461db0af52e18d02e38fe54d7afbfffe435
Benjamin Bergman <bbergman@iders.ca>
2012-07-10 15:59:06 -0500

    some improvements to completion colouring

--- completion.zsh
+++ completion.zsh
@@ -1,6 +1,8 @@
 # colorful listings
-zmodload -i zsh/complist
+zmodload -a complist
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors 'reply=( "=(#b)(*$VAR)(?)*=00=$color[green]=$color[bg-green]" )'
+
 
 autoload -U compinit
 compinit
@@ -10,6 +12,10 @@ zstyle ':completion::complete:*' use-cache 1
 
 # highlight current completion option in a menu and immediately select first option
 zstyle ':completion:*' menu yes select
+
+# pid completion for kill command
+zstyle ':completion:*:*:kill:*' menu yes select
+zstyle ':completion:*:kill:*' force-list always
 
 ## From Burke's config
 #zstyle ':completion:*:manuals'    separate-sections true
